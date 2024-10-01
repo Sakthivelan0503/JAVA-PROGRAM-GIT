ebfe4725b0e79331a966229ccaee5e4b013002a3
Sakthivelan0503 <velanm669@gmail.com>
2024-10-01 12:05:57 +0530

    init

---
+++ Accenture/dice.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class dice
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int count=0;
+        for(int i=1;i<=6;i++)
+        {
+            for(int j=1;j<=6;j++)
+            {
+                if(i+j==a)
+                count++;
+            }
+        }
+        System.out.print(count);
+
+    }
+}
---
+++ Accenture/rat_comsumption.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class rat_comsumption {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        rat_comsumption rt=new rat_comsumption();
+        int rat=scan.nextInt();
+        int unit=scan.nextInt();
+        int h=scan.nextInt();
+        int hf[]=new int[h];
+        for(int i=0;i<h;i++)
+            hf[i]=scan.nextInt();
+        rt.find(rat,unit,h,hf);
+        
+    }
+    void find(int rat,int unit,int h,int[] hf){
+
+        if(h==0)
+        System.out.println(-1+" there in no house");
+        int tf=0;
+        for(int i=0;i<h;i++)
+        tf=tf+hf[i];
+        if(tf<=(rat*unit))
+            System.out.print(0+" there is no food");
+
+            
+            int t=rat*unit;
+        int sum=0;
+        for(int i=0;i<h;i++){
+            if(sum<=t){
+                sum=sum+hf[i];
+            }
+            
+            else{
+                System.out.print(i);
+            break;
+        }
+    }
+}
+}
